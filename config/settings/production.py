9b9f4170684be371a05772e89e283ad7d1f5024f
John Anthony San Diego <janthonysandiego@gmail.com>
2026-06-29 01:34:46 +0800

    Initial Deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,3 +15,7 @@ STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
 }
+CSRF_TRUSTED_ORIGINS = [
+    "https://examiq.onrender.com",
+]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
